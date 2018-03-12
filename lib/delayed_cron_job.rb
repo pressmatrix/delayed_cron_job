240391a13c82eb097806f545abf67d38bfd3e34e
Alexander Hoth <alexander.hoth@pressmatrix.de>
2018-03-12 11:54:05 +0100

    Ignoring Bundler::GemRequireError on attr_accessible check

--- lib/delayed_cron_job.rb
+++ lib/delayed_cron_job.rb
@@ -19,15 +19,23 @@ end
 
 if defined?(Delayed::Backend::Mongoid)
   Delayed::Backend::Mongoid::Job.field :cron, :type => String
-  # Delayed::Backend::Mongoid::Job.attr_accessible(:cron) if Delayed::Backend::Mongoid::Job.respond_to?(:attr_accessible)
+  begin
+    Delayed::Backend::Mongoid::Job.attr_accessible(:cron) if Delayed::Backend::Mongoid::Job.respond_to?(:attr_accessible)
+  rescue Bundler::GemRequireError
+    # ignore ...
+  end
   Delayed::Backend::Mongoid::Job.send(:include, DelayedCronJob::Backend::UpdatableCron)
 end
 
 if defined?(Delayed::Backend::ActiveRecord)
   Delayed::Backend::ActiveRecord::Job.send(:include, DelayedCronJob::Backend::UpdatableCron)
-  # if Delayed::Backend::ActiveRecord::Job.respond_to?(:attr_accessible)
-  #   Delayed::Backend::ActiveRecord::Job.attr_accessible(:cron)
-  # end
+  begin
+    if Delayed::Backend::ActiveRecord::Job.respond_to?(:attr_accessible)
+      Delayed::Backend::ActiveRecord::Job.attr_accessible(:cron)
+    end
+  rescue Bundler::GemRequireError
+    # ignore ...
+  end
 end
 
 Delayed::Worker.plugins << DelayedCronJob::Plugin
